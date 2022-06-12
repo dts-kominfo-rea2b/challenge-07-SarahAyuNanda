df858323f5209d64af1a3955d234f70c9583eef3
Sarah Ayu Nanda <nanda.sarahayu@gmail.com>
2022-06-12 12:12:53 +0700

    main: create handler to count the movie response using promise

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async value => {
+  let [dataIXX, dataVGC] = await Promise.all([promiseTheaterIXX(), promiseTheaterVGC()])
+    .then(res => res)
+    .catch(err => console.log(err))
+
+  let response = 0
+  let allData = []
+  if (Array.isArray(dataIXX) && Array.isArray(dataVGC)) {
+    allData = [...dataIXX, ...dataVGC]
+    response = allData.filter(item => item.hasil === value).length
+  }
+  return response
+};
 
 module.exports = {
   promiseOutput,
